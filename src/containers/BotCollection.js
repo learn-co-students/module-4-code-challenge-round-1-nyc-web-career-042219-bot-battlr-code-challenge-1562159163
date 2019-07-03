a1f3a27feabe1e27292db111ba38ce6420b157e1
Christian Duncan <christianeduncan3@gmail.com>
2019-07-03 10:38:53 -0400

    did refactor deliverable

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,10 +1,17 @@
 import React from "react";
 import BotCard from "../components/BotCard";
+import BotSpecs from "../components/BotSpecs"
 
 class BotCollection extends React.Component {
   //your code here
 
-  
+  state = {
+	  goBack: false
+  }
+
+  toggleGoBack = () => {
+	  this.setState({goBack: true})
+  }
 
   render(){
 	//   console.log(this.props.botCollection)
@@ -12,7 +19,11 @@ class BotCollection extends React.Component {
   	  <div className="ui four column grid">
     		<div className="row">
     		  {this.props.botCollection.map(bot => {
-				  return <BotCard key={bot.id} bot={bot} handleClick={this.props.handleClick}/>
+				  if(this.state.goBack){ 
+				  return <BotCard key={bot.id} bot={bot} handleClick={this.props.handleClick} />
+				  } else {
+				  return <BotSpecs toggleGoBack={this.toggleGoBack} goBack={this.state.goBack} key={bot.id} bot={bot} handleClick={this.props.handleClick}/>
+				  }
 			  })}
     		  Collection of all bots
     		</div>
