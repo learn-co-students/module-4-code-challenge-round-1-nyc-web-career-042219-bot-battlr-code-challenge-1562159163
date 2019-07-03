3a91cd781c911993036aaa8f4ba9a6e14e35d1ea
gavinoconnor <gedmund@gmail.com>
2019-07-03 11:03:45 -0400

    Done

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -2,13 +2,24 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class BotCollection extends React.Component {
-  //your code here
+
+  handleClick = (botObj) => {
+    console.log("clicked:", botObj.id)
+    // let targetBot = this.props.bots.map(bot => bot.id === botObj.id))
+    //   this.props.botArmy.push(targetBot)
+    // once bot is selected, push to "Your Army" array
+  }
+
+
 
   render(){
+    console.log(this.props.bots)
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
+          {this.props.bots.map(bot => {
+            return <BotCard key={bot.id} {...bot} handleClick={this.handleClick}/>
+          })}
     		  Collection of all bots
     		</div>
   	  </div>
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,42 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
+
+const URL = "https://bot-battler-api.herokuapp.com/api/v1/bots"
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  state = {
+    bots: [],
+    botArmy: []
+  }
+
+  componentDidMount() {
+    fetch(URL)
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        bots: data
+        })
+      })
+    }
+
+    // handleClick = (botObj) => {
+    //   console.log("clicked:", botObj.id)
+    //   console.log(this.state.botArmy)
+    //   // let targetBot = this.props.bots.map(bot => bot.id === botObj.id))
+    //   //   this.props.botArmy.push(targetBot)
+    // }
+
+    // Moved handleClick to parent of Collection and Army, didn't have time to refactor child components...
+
+
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy botArmy={this.state.botsArmy}/>
+        <BotCollection bots={this.state.bots} />
       </div>
     );
   }
